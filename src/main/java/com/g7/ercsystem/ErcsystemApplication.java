5c9c95858d1d8af4ada7803f79348bcab8de37e6
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-06-06 17:02:24 +0530

    create method add initial users and roles for db

--- src/main/java/com/g7/ercsystem/ErcsystemApplication.java
+++ src/main/java/com/g7/ercsystem/ErcsystemApplication.java
@@ -1,26 +1,42 @@
 package com.g7.ercsystem;
 
+import com.g7.ercsystem.assembler.AuthAssembler;
+import com.g7.ercsystem.payload.requests.SignUpRequest;
 import com.g7.ercsystem.repository.RoleRepository;
+import com.g7.ercsystem.repository.UserRepository;
 import com.g7.ercsystem.rest.auth.model.EnumRole;
+import com.g7.ercsystem.rest.auth.model.MemberDetails;
 import com.g7.ercsystem.rest.auth.model.Role;
+import com.g7.ercsystem.rest.auth.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
+@EnableAsync
 public class ErcsystemApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	@Autowired
 	private RoleRepository roleRepository;
+	@Autowired
+	private AuthAssembler authAssembler;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErcsystemApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		if(roleRepository.findAll().isEmpty()){
 			Role role1 = new Role(EnumRole.ROLE_APPLICANT);
 			Role role2 = new Role(EnumRole.ROLE_REVIEWER);
@@ -34,5 +50,22 @@ public class ErcsystemApplication extends SpringBootServletInitializer implement
 			roleRepository.save(role4);
 			roleRepository.save(role5);
 		}
+
+		if(userRepository.findAll().isEmpty()){
+			SignUpRequest signUpRequest = new SignUpRequest();
+			Set<String> roles = new HashSet<>();
+			roles.add("admin");
+			roles.add("secretary");
+			roles.add("applicant");
+			roles.add("clerk");
+			roles.add("reviewer");
+			signUpRequest.setEmail("admin@gmail.com");
+			signUpRequest.setPassword("12345678");
+			signUpRequest.setRoles(roles);
+			signUpRequest.setMemberDetails(new MemberDetails("Sandaruwan lakshitha","Matara","987654321V"));
+			userRepository.save(authAssembler.SignUpRequestToUserEntity(signUpRequest));
+		}
+
+
 	}
 }
